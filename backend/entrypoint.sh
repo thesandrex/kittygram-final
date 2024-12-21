acb0b5996ddca04e2e7567eb2ba0b812642acd64
thesandrex <aleksanya2012@yandex.ru>
2024-12-21 18:00:34 +0400

    django import fix

---
+++ backend/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "${0}: running migrations"
+python manage.py makemigrations --merge
+python manage.py migrate --noinput
+
+echo "${0}: collecting statics"
+
+python manage.py collectstatic --noinput
+
+echo "${0}: start backend app"
+
+python manage.py runserver 0.0.0.0:8000
