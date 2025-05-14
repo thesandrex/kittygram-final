69e1a5785dafe7ff9a6523447f842baf5e439d3b
Alexander Smirnov <83784045+thesandrex@users.noreply.github.com>
2025-05-14 21:39:51 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.90"
+    }
+  }
+}
+
 provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
