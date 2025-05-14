ea15e4a56e4f1d35538234aafaa0e01288342822
Alexander Smirnov <83784045+thesandrex@users.noreply.github.com>
2025-05-14 21:47:37 +0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+}
+
+variable "folder_id" {
+  description = "Yandex Folder ID"
+  type        = string
+}
+
+variable "token" {
+  description = "Yandex OAuth Token"
+  type        = string
+}
+
+variable "vm_user" {
+  description = "Username to create on VM"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VM access"
+  type        = string
+}
