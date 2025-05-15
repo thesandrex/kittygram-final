c6214291165e408a09f3461baeee1f7d08293528
Alexander Smirnov <83784045+thesandrex@users.noreply.github.com>
2025-05-15 21:29:27 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,6 @@ terraform {
     bucket     = "crocksgift-kittygram-terraform-state"
     key        = "terraform/state.tfstate"
     region     = "ru-central1"
-    access_key = var.access_key
-    secret_key = var.secret_key
     skip_region_validation      = true
     skip_credentials_validation = true
   }
