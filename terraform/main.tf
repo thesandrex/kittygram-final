8eab71ee0bf0b8e07a5c5c4d425b482732833601
Alexander Smirnov <83784045+thesandrex@users.noreply.github.com>
2025-05-15 22:07:52 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.90"
     }
   }
+  required_version = ">= 0.13"
+
   backend "s3" {
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
@@ -13,6 +14,8 @@ terraform {
     key          = "terraform/state.tfstate"
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
