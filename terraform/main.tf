2810d351605b01062bc64e0297e02b077a2b0531
Alexander Smirnov <83784045+thesandrex@users.noreply.github.com>
2025-05-15 22:26:40 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ terraform {
 provider "yandex" {
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
-  token     = var.token
+  service_account_key_file = "key.json"
 }
 
 resource "yandex_vpc_network" "kittygram_network" {
