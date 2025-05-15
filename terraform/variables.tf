dba59997189eb0deaec5b56de352cd2b3e8391d9
Alexander Smirnov <83784045+thesandrex@users.noreply.github.com>
2025-05-15 23:09:26 +0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,21 +8,6 @@ variable "folder_id" {
   type        = string
 }
 
-variable "access_key" {
-  description = "Yandex Folder ID"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "Yandex Folder ID"
-  type        = string
-}
-
-variable "token" {
-  description = "Yandex OAuth Token"
-  type        = string
-}
-
 variable "vm_user" {
   description = "Username to create on VM"
   type        = string
